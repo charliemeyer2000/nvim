064440d69a86f1c5a8f57eb1921dd6bdf9bf5fb6
Charlie Meyer <charlie.g.meyer@icloud.com>
2024-01-05 19:05:28 -0500

    better, simple lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,91 +1,72 @@
 return {
   {
     "williamboman/mason.nvim",
-    lazy = false,
     config = function()
-      require("mason").setup()
+      require("mason").setup({})
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {},
+      })
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local keymap = vim.keymap
-      local opts = { noremap = true, slient = true }
-      local on_attach = function(client, bufnr)
-        opts.bufnr = bufnr
-        opts.desc = "Show LSP references"
-        keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-
-        opts.desc = "Show documentation for what is under cursor"
---        keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        
-      end
-      -- setup python
-      require("lspconfig").pyright.setup({
-        --capabilities = capabilities,
-        on_attach = on_attach
-      })
-      -- setup lua
-      require("lspconfig").lua_ls.setup({
-        --capabilities = capabilities,
-      })
-      -- setup typescript
-      require("lspconfig").tsserver.setup({
-        -- capabilities = capabilities,
-      })
-      -- setup html
-      require("lspconfig").html.setup({
-        -- capabilities = capabilities,
-      })
-      -- setup css
-      require("lspconfig").cssls.setup({
-        capabilities = capabilities
-      })
-      -- setup json
-      require("lspconfig").jsonls.setup({
-        -- capabilities = capabilities
-      })
-      -- setup yaml
-      require("lspconfig").yamlls.setup({
-        -- capabilities = capabilities
-      })
-      -- setup docker
-      require("lspconfig").dockerls.setup({
-        -- capabilities = capabilities
-      })
-      -- setup bash
-      require("lspconfig").bashls.setup({
-        -- capabilities = capabilities
-
-      })
-      -- setup vim
-      require("lspconfig").vimls.setup({
-        -- capabilities = capabilities
-      })
-      -- setup c
-      require("lspconfig").clangd.setup({
-        -- capabilities = capabilities
-      })
-      -- setup cpp
-      require("lspconfig").clangd.setup({
-        -- capabilities = capabilities
-      })
-      -- setup rust
-      require("lspconfig").rust_analyzer.setup({
-        -- capabilities = capabilities
-      })
-      -- setup go
-      require("lspconfig").gopls.setup({
-        -- capabilities = capabilities
-      })
+      require("lspconfig").pyright.setup({})
+      require("lspconfig").tsserver.setup({})
+      require("lspconfig").gopls.setup({})
+      require("lspconfig").rust_analyzer.setup({})
+      require("lspconfig").clangd.setup({})
+      require("lspconfig").vimls.setup({})
+      require("lspconfig").bashls.setup({})
+      require("lspconfig").yamlls.setup({})
+      require("lspconfig").jsonls.setup({})
+      require("lspconfig").html.setup({})
+      require("lspconfig").cssls.setup({})
+      require("lspconfig").dockerls.setup({})
+      require("lspconfig").terraformls.setup({})
+      require("lspconfig").graphql.setup({})
+      require("lspconfig").lua_ls.setup({})
+      require("lspconfig").vimls.setup({})
+      require("lspconfig").vuels.setup({})
+      require("lspconfig").dartls.setup({})
+      require("lspconfig").jdtls.setup({})
+      require("lspconfig").solargraph.setup({})
+      require("lspconfig").elixirls.setup({})
+      require("lspconfig").clojure_lsp.setup({})
+      require("lspconfig").nimls.setup({})
+      require("lspconfig").hls.setup({})
+      require("lspconfig").cmake.setup({})
+      require("lspconfig").r_language_server.setup({})
+      require("lspconfig").svelte.setup({})
+      require("lspconfig").tailwindcss.setup({})
+      require("lspconfig").sorbet.setup({})
+      require("lspconfig").leanls.setup({})
+      require("lspconfig").sqlls.setup({})
+      require("lspconfig").dartls.setup({})
+      require("lspconfig").julials.setup({})
+      require("lspconfig").jedi_language_server.setup({})
+      require("lspconfig").nimls.setup({})
+      require("lspconfig").hls.setup({})
+      require("lspconfig").cmake.setup({})
+      require("lspconfig").r_language_server.setup({})
+      require("lspconfig").svelte.setup({})
+      require("lspconfig").tailwindcss.setup({})
+      require("lspconfig").sorbet.setup({})
+      require("lspconfig").leanls.setup({})
+      require("lspconfig").sqlls.setup({})
+      -- keybindings
+      vim.keymap.set('n', "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { noremap = true, silent = true })
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { noremap = true, silent = true })
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true })
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap = true, silent = true })
     end,
-  }
+  },
 }
