6c4951296e89a540cfe208e1dd0a19215f8475d3
Charlie Meyer <charlie.g.meyer@icloud.com>
2024-01-06 15:15:19 -0700

    vimbegood and nohlsearch

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -10,7 +10,7 @@ return {
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
       -- also set the background to none when in split mode with multiple windows
       vim.cmd [[ au VimEnter,WinEnter,BufWinEnter * if winnr('$') > 1 | setlocal winhighlight=Normal:NormalFloat,NormalNC:NormalFloat | endif ]]
-      
     end
   }
 }
+
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,14 +1,22 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set smarttab")
+vim.cmd("set autoindent")
+
+
 vim.g.mapleader = " "
 vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 -- make shift + tab be unindent
 vim.api.nvim_set_keymap("i", "<S-Tab>", "<C-D>", { noremap = true, silent = true })
 -- add line numbers
-vim.cmd("set number")
+vim.cmd("set relativenumber number")
 -- make yank also copy to system clipboard
 vim.cmd("set clipboard+=unnamedplus")
-vim.cmd("set autoindent")
+-- jj to exit insert
+vim.cmd("inoremap jj <Esc>")
+
+-- turn off highlight until next search
+vim.cmd("set nohlsearch")
