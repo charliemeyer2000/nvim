00ba32a8506949346fbae986a04fffa270837c39
Charlie Meyer <charlie.g.meyer@icloud.com>
2025-04-03 09:53:20 -0700

    feat: add mini pairs

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require('telescope.builtin')
+        local telescope = require('telescope')
+
+        -- Configure telescope to respect gitignore
+        telescope.setup({
+            defaults = {
+                file_ignore_patterns = {},
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
+                    "--glob=!.git/",
+                },
+            },
+            pickers = {
+                find_files = {
+                    find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--hidden", "--exclude", ".git" },
+                    follow = true,
+                    hidden = true,
+                    no_ignore = false,
+                },
+                live_grep = {
+                    additional_args = function()
+                        return { "--hidden", "--no-ignore-vcs" }
+                    end,
+                },
+            },
+        })
+
+        -- Set up keymaps
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end
+}
