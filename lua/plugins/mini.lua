49caa29abe424597b7f39a8e00a9e20b7ffe1d95
Charlie Meyer <charlie.g.meyer@icloud.com>
2025-04-08 14:29:20 -0700

    feat: python docstrings configuration in mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,11 @@
 return {
     "echasnovski/mini.pairs", version = "*",
     config = function()
-        require("mini.pairs").setup()
+        require("mini.pairs").setup({
+            -- Disable auto-pairing for triple quotes in Python
+            mappings = {
+                ['"'] = { action = 'open', pair = '""', neigh_pattern = '[^\\].' },
+            }
+        })
     end
 }
